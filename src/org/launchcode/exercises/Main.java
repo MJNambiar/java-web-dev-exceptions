3937db87a9796901a19859d180293ddf70f7cb2d
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-09-26 12:03:39 -0500

    exercises started

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,13 +1,26 @@
 package org.launchcode.exercises;
 
+import org.launchcode.Temperature.Temperature;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Test out your Divide() function!
+        int x;
+        int y;
+        Scanner input;
+        input = new Scanner(System.in);
+        System.out.println("Enter points earned: ");
+        x = input.nextInt();
+        System.out.println("Enter possible points: ");
+        y = input.nextInt();
+        input.close();
+        Divide(x,y);
+
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -18,14 +31,38 @@ public class Main {
         // Test out your CheckFileExtension() function!
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static void Divide(int x, int y) {
+        int result = x/y;
+
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("You cannot divide by zero");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(result);
     }
 
     public static int CheckFileExtension(String fileName)
     {
-        // Write code here!
+        if (fileName.endsWith(".java")) {
+            return 1;
+
+        } else if (!fileName.endsWith(".java")) {
+            return 0;
+
+        } else if (fileName == null || fileName == "") {
+            try {
+                throw new ArithmeticException("Need to add the proper file name.");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+            return -1;
+
+        } else {
+            return 0;
+        }
     }
 
 }
